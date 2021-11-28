aeae82808c46216a250cee9bb16d55979b0f7a37
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-11-28 11:21:43 -0500

    Make turtle automatically clear it's inventory on startup and add panic condition

--- random/shear.lua
+++ random/shear.lua
@@ -1,11 +1,28 @@
 -- suck 2 iron from chest make shear drop down
 
+for i=1,16 do
+  if turtle.getItemDetail(i) then
+    turtle.select(i)
+    turtle.dropUp()
+  end
+end
+turtle.select(1)
+
 while true do
   turtle.select(1)
-  turtle.suck(2)
-  turtle.transferTo(6,1)
-  turtle.craft()
-  while not turtle.dropDown() do
-    sleep()
+  if turtle.suck(2) then
+    turtle.transferTo(6,1)
+    turtle.craft()
+    while not turtle.dropDown() do
+      sleep()
+    end
+  else -- PANIC
+    for i=1,16 do
+      if turtle.getItemDetail(i) then
+        turtle.select(i)
+        turtle.dropUp()
+      end
+    end
+    turtle.select(1)
   end
 end
