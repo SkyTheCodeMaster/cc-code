10654345f686021a4334ff9f2d7715ba67e73092
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-06-30 22:50:39 -0400

    Optimize the player tracker module

--- overlord/modules/playerTracker.lua
+++ overlord/modules/playerTracker.lua
@@ -12,23 +12,30 @@ local function centerWrite(txt,y,t)
   t.write(txt)
 end
 
+local win = window.create(mon,1,1,mon.getSize())
+
 local function main()
   while true do
-    mon.setTextColour(colours.white)
-    mon.setBackgroundColour(colours.blue)
-    mon.clear()
+    win.setVisible(false)
 
-    centerWrite("Player Position",1,mon)
+    win.setTextColour(colours.white)
+    win.setBackgroundColour(colours.blue)
+    win.clear()
 
-    mon.setCursorPos(1,2)
-    mon.write(("-"):rep(w))
+    centerWrite("Player Position",1,win)
+
+    win.setCursorPos(1,2)
+    win.write(("-"):rep(w))
 
     local players = pd.getOnlinePlayers()
     for i,v in ipairs(players) do
       local pos = pd.getPlayerPos(v)
-      centerWrite(("%s: %d, %d, %d"):format(v,pos.x,pos.y,pos.z),3+i,mon)
+      if pos then
+        centerWrite(("%s: %d, %d, %d"):format(v,pos.x,pos.y,pos.z),3+i,win)
+      end
     end
 
+    win.setVisible(true)
     sleep(5)
   end
 end
