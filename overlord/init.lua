e36caa670fcee1789f96239612a8fe3632757853
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-06-30 20:17:15 -0400

    Fix _ENV stuff

--- overlord/init.lua
+++ overlord/init.lua
@@ -5,12 +5,7 @@ for _,file in pairs(fs.list("modules")) do
   local f = fs.open(fs.combine("modules",file),"r")
   local contents = f.readAll()
   f.close()
-  local env = {
-    shell=shell,
-    multishell=multishell,
-  }
-  env.require,env.package = r.make(env,"/")
-  table.insert(modules,function() pcall(load(contents,"="..file,"t",env)) end)
+  table.insert(modules,function() load(contents,"="..file,"t",_ENV)() end)
 end
 
 parallel.waitForAll(table.unpack(modules))
