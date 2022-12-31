6dea5046e615dd17b4d77cbd560e70c2edac89dd
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-12-31 00:51:51 -0500

    Add concrete processor

---
+++ survival/concrete.lua
@@ -0,0 +1,39 @@
+-- Pulls powder from top chest, places in front, mines, pushes to bottom chest.
+
+local function dropComplete()
+  for i=1,16 do
+    local data = turtle.getItemDetail(i)
+    if data and data.name.match("minecraft:%a+_concrete$") then
+      turtle.select(i)
+      turtle.dropDown(64)
+    end
+  end
+  turtle.select(1)
+end
+
+local function suck()
+  while turtle.suckUp(64) do end
+  dropComplete()
+  turtle.select(1)
+  turtle.dropUp(64) -- Make sure we have space to pick up the completed concrete.
+end
+
+local function process()
+  for i=1,16 do
+    local data = turtle.getItemDetail(i)
+    if data and data.name.match("minecraft:%a+_concrete_powder") then
+      turtle.select(i)
+      while turtle.getItemCount(i) ~= 0 do
+        turtle.place()
+        turtle.dig()
+        turtle.suck()
+      end
+    end
+  end
+end
+
+while true do
+  suck()
+  process()
+  dropComplete()
+end
