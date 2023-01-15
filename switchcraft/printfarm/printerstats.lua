6c98ca247b6da140e0cdd2e86cc1498ed4640534
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2023-01-14 19:14:08 -0500

    change everything

--- switchcraft/printfarm/printerstats.lua
+++ switchcraft/printfarm/printerstats.lua
@@ -9,6 +9,7 @@ monitor.setBackgroundColour(colours.black)
 monitor.setTextColour(colours.white)
 monitor.clear()
 local w,h = monitor.getSize()
+local superWin = window.create(monitor,1,1,w,h)
 
 local printers = {peripheral.find("3d_printer")}
 
@@ -27,51 +28,60 @@ for i,printer in ipairs(printers) do
   end
   rowLength = rowLength + 14
 
-  local win = window.create(monitor,column*14+1,row*8+1,14,8)
+  local win = window.create(superWin,column*14+1,row*8+1,14,8)
   local obj = {win=win,printer=printer,id=i,bars={}}
   pstats[#pstats+1] = obj
   column = column + 1
 end
 
-local function displayStats(obj)
-  local win = obj.win
-  local printer = obj.printer
-  local id = obj.id
-  if not obj.bars.status then obj.bars.status = pg.create(2,3,13,1,colours.lime,colours.red,0,win) end
-  if not obj.bars.ink then obj.bars.ink = pg.create(2,5,13,1,colours.lime,colours.red,0,win) end
-  if not obj.bars.chamelium then obj.bars.chamelium = pg.create(2,7,13,1,colours.lime,colours.red,0,win) end
+local function getStats(objs)
+  local stats = {}
+  for _,obj in pairs(objs) do
+    local stat = {}
+    stat.bars.status = pg.create(2,3,13,1,colours.lime,colours.red,0,obj.win)
+    stat.bars.ink = pg.create(2,5,13,1,colours.lime,colours.red,0,obj.win)
+    stat.bars.chamelium = pg.create(2,7,13,1,colours.lime,colours.red,0,obj.win)
+    local printer = obj.printer
+    local c,cMax = printer.getChameliumLevel() -- chamelium, chamelium max
+    local i,iMax = printer.getInkLevel() -- ink, ink max
+    local status,progress = printer.status() -- Printer status and progress
+    stat.c,stat.cMax = c,cMax
+    stat.i,stat.iMax = i,iMax
+    stat.status,stat.progress = status,progress
+    stat.win = obj.win
+    stat.id = obj.id
+    stats[#stats+1] = stat
+  end
+  return stats
+end
 
-  local c,cMax = printer.getChameliumLevel() -- chamelium, chamelium max
-  local i,iMax = printer.getInkLevel() -- ink, ink max
-  local status,progress = printer.status() -- Printer status and progress
-  win.setVisible(false)
+local function drawStats(stats)
+  for _,stat in stats do
+    local win = stat.win
 
-  win.setCursorPos(2,1)
-  win.write("Printer #"..id)
-
-  win.setCursorPos(2,2)
-  win.write("Status: "..(status or "UNK"))
-
-  win.setCursorPos(2,4)
-  win.write("Ink:")
-
-  win.setCursorPos(2,6)
-  win.write("Chamelium:")
-
-  obj.bars.status:update((progress or 0))
-  obj.bars.ink:update((i/iMax)*100)
-  obj.bars.chamelium:update((c/cMax)*100)
-
-  win.setVisible(true)
+    win.setVisible(false)
+  
+    win.setCursorPos(2,1)
+    win.write("Printer #"..stat.id)
+  
+    win.setCursorPos(2,2)
+    win.write("Status: "..(stat.status or "UNK"))
+  
+    win.setCursorPos(2,4)
+    win.write("Ink:")
+  
+    win.setCursorPos(2,6)
+    win.write("Chamelium:")
+  
+    stat.bars.status:update((stat.progress or 0))
+    stat.bars.ink:update((stat.i/stat.iMax)*100)
+    stat.bars.chamelium:update((stat.c/stat.cMax)*100)
+  
+    win.setVisible(true)
+  end
 end
 
 while true do
-  local funcs = {}
-  for _,p in pairs(pstats) do
-    funcs[#funcs+1] = function()
-      displayStats(p)
-    end
-  end
-  parallel.waitForAll(table.unpack(funcs))
+  drawStats(getStats(pstats))
   sleep(1)
 end
