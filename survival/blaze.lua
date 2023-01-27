2e0bfad01663aaa8e97ea4dc87e1fad03b29e4f6
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2023-01-27 13:18:18 -0500

    Add blaze farm

---
+++ survival/blaze.lua
@@ -0,0 +1,27 @@
+-- uses a farm to funnel blazes ontop of the turtles
+-- then they kill the blazes and hoppers suck out blaze rods.
+
+term.setBackgroundColour(colours.blue)
+term.setTextColour(colours.white)
+term.clear()
+
+local function centerWrite(txt,y,t)
+  t = t or term
+  local ox,oy = t.getCursorPos()
+  y = y or oy
+  local width = t.getSize()
+  t.setCursorPos(math.ceil((width/2)-(txt:len()/2)),y)
+  t.write(txt)
+  t.setCursorPos(ox,oy)
+end
+
+centerWrite("Skynet Blaze Farm",1,term)
+term.setCursorPos(1,2)
+local w = term.getSize()
+term.write(string.rep("-",w))
+
+while true do
+  turtle.attackUp()
+  turtle.suckUp(64)
+  sleep()
+end
