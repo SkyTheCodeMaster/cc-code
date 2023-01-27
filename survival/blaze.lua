2235499d9e11fa18880349b47e447ba29904d971
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2023-01-27 18:03:38 -0500

    Add item draining to blaze farm

--- survival/blaze.lua
+++ survival/blaze.lua
@@ -5,6 +5,9 @@ term.setBackgroundColour(colours.blue)
 term.setTextColour(colours.white)
 term.clear()
 
+local chest = peripheral.find("sc-goodies:diamond_chest")
+local selfName = peripheral.wrap("bottom").getNameLocal()
+
 local function centerWrite(txt,y,t)
   t = t or term
   local ox,oy = t.getCursorPos()
@@ -23,5 +26,10 @@ term.write(string.rep("-",w))
 while true do
   turtle.attackUp()
   turtle.suckUp(64)
+  for i=1,16 do
+    if turtle.getItemDetail(i) then
+      chest.pullItems(selfName,i)
+    end
+  end
   sleep()
 end
