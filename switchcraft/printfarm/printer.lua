53b66015646156b50dc7dac941154910c8783d1f
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2023-02-04 12:39:23 -0500

    Add start of print selection box

--- switchcraft/printfarm/printer.lua
+++ switchcraft/printfarm/printer.lua
@@ -1,5 +1,6 @@
 -- This queues and manages prints on the printer
 local pg = require("progressbar")
+local button = require("button")
 
 local monitor = peripheral.wrap("monitor_368")
 
@@ -73,6 +74,99 @@ local function printManager()
   end
 end
 
+local function drawBox(x,y,w,h,colour,filled,t)
+  t = t or term
+  for i = y, y+h-1 do
+    if i == y or i == y+h-1 then
+      t.setCursorPos(1,i)
+      t.blit((" "):rep(w),colour:rep(w),colour:rep(w))
+    else
+      if not filled then
+        t.setCursorPos(1,i)
+        t.blit(" ",colour,colour)
+        t.setCursorPos(x+w-1,i)
+        t.blit(" ",colour,colour)
+      else
+        t.setCursorPos(1,i)
+        t.blit((" "):rep(w),colour:rep(w),colour:rep(w))
+      end
+    end
+  end
+end
+
+local function buttonCallback(file,win)
+  local w,h = win.getSize()
+
+  -- Collect information from the file.
+  -- Load the file
+  local f = assert(fs.open(file,"r"))
+  -- deserialize it
+  local data = textutils.unserializeJSON(f.readAll())
+  -- Now close the file.
+  f.close()
+
+  -- We can extract the label.
+  local label = data.label or "unlabelled print"
+  -- We can start going through and calculating volume of the print for ink and chamelium costs.
+  local volume = 0
+  local surface = 0
+  for _,shape in pairs(data.shapesOff) do
+    volume = volume + (
+      shape[4] - shape[1] *
+      shape[5] - shape[2] *
+      shape[6] - shape[3]
+    )
+    surface = surface + 2*(
+      shape[4] - shape[1] +
+      shape[5] - shape[2] +
+      shape[6] - shape[3]
+    )
+  end
+  for _,shape in pairs(data.shapesOn) do
+    volume = volume + (
+      shape[4] - shape[1] *
+      shape[5] - shape[2] *
+      shape[6] - shape[3]
+    )
+    surface = surface + 2*(
+      shape[4] - shape[1] +
+      shape[5] - shape[2] +
+      shape[6] - shape[3]
+    )
+  end
+
+  local redstoneCost = 300
+  local noclipMultiplier = 2
+
+
+  local chamelium = math.floor(
+      (
+        (volume/2) + 
+        (data.redstoneLevel > 0 and redstoneCost or 0)
+      ) * 
+      (
+        (data.collideWhenOff or data.collideWhenOn) and noclipMultiplier or 1
+      )
+    )
+  local ink = math.floor((surface/6))
+
+  -- Now we can overlay a box
+  drawBox(1,1,18,5,"8",true,win)
+  -- Print the label
+  win.setCursorPos(2,2)
+  win.write(label:sub(1,16))
+  -- Now draw a box for the quantity. We're gonna have a sanity max of 9999,
+  -- which would take 13 minutes on a 64 printer array. It also overflows a
+  -- diamond chest, so realistically the max is 6912
+  drawBox(2,3,4,1,"7",true,win)
+  win.setCursorPos(7,3)
+  win.write("CHA: "..chamelium)
+  win.setCursorPos(7,4)
+  win.write("INK: "..ink)
+end
+
+local buttons = {}
+
 local function selectionManager()
   -- Here we prompt the user to actually figure out what we're printing.
   local win = window.create(term.current(),1,1,term.getSize())
@@ -83,35 +177,26 @@ local function selectionManager()
     win.setTextColour(colours.white)
 
     -- Draw a nice little box around the file selection window.
-    for y = 1, h-3 do
-      if y == 1 or y == h-3 then
-        win.setCursorPos(1,y)
-        win.blit((" "):rep(w),("b"):rep(w),("b"):rep(w))
-      else
-        win.setCursorPos(1,y)
-        win.blit(" ","b","b")
-        win.setCursorPos(w,y)
-        win.blit(" ","b","b")
-      end
-    end
+    drawBox(1,1,w,h-3,"b",false,win)
 
     -- Draw a nice little box around the file selection window.
-    for y = h-3, h do
-      if y == h-3 or y == h or y == h-1 then
-        win.setCursorPos(1,y)
-        win.blit((" "):rep(w),("b"):rep(w),("b"):rep(w))
-      else
-        win.setCursorPos(1,y)
-        win.blit(" ","b","b")
-        win.setCursorPos(w,y)
-        win.blit(" ","b","b")
-      end
+    drawBox(1,h-3,w,h,"b",false,win)
+    win.setCursorPos(1,h-1)
+    win.blit((" "):rep(w),("b"):rep(w),("b"):rep(w))
+
+    -- Void the buttons table.
+    for _,id in pairs(buttons) do
+      button.delete(id)
     end
+    buttons = {}
 
     local files = fs.list("disk")
     for i=1,#files do
       win.setCursorPos(2,1+i)
       win.write(files[i])
+      -- Now we have to go and rebuild the buttons if anything has changed.
+      local btn = button.new(2,1+i,w-2,1,function() buttonCallback(files[i],win) end)
+      buttons[#buttons+1] = btn
     end
 
     win.setVisible(true)
