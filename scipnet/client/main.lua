cdf4c8d40b4ceeaed234bc09b889cc00a037e7bd
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2023-02-08 21:25:59 -0500

    Add SCiPNet Client

---
+++ scipnet/client/main.lua
@@ -0,0 +1,52 @@
+local coro = require("libraries.coro")
+
+-- Load main modules
+local _modules = fs.list("modules")
+local modules = {}
+-- Get proper filepath
+for _,fileName in ipairs(_modules) do
+  table.insert(modules,fs.combine("modules",fileName))
+end
+
+local funcs = {}
+for _,file in pairs(modules) do
+  ---@diagnostic disable-next-line: redefined-local
+  local f = fs.open(file,"r")
+  local content = f.readAll()
+  f.close()
+  local code = load(content,"="..file,"t",_ENV)
+  if code then
+    table.insert(funcs,function()
+      code()
+    end)
+  end
+end
+
+for _,func in pairs(funcs) do
+  coro.newCoro(func)
+end
+
+term.setBackgroundColour(colours.blue)
+term.setTextColour(colours.white)
+term.clear()
+local function centerWrite(txt,y,t)
+  t = t or term
+  local ox,oy = t.getCursorPos()
+  y = y or oy
+  local width = t.getSize()
+  t.setCursorPos(math.ceil((width/2)-(txt:len()/2)),y)
+  t.write(txt)
+  t.setCursorPos(ox,oy)
+end
+centerWrite("SCiPNet Client",1)
+term.setCursorPos(1,2)
+local w = term.getSize()
+term.write(("-"):rep(w))
+term.setCursorPos(1,3)
+term.write("Loaded modules:")
+for i,file in ipairs(modules) do
+  term.setCursorPos(1,i+3)
+  term.write(fs.getName(file))
+end
+
+coro.runCoros()
---
+++ scipnet/client/modules/gps.lua
@@ -0,0 +1,27 @@
+-- This simply reports the GPS position of the user to the main server
+
+local modem = peripheral.find("modem")
+local enc = require("libraries.encryption")
+local sha=require("libraries.sha256")
+local keyf = fs.open(".key","r")
+local key = keyf.readAll()
+keyf.close()
+local ni = peripheral.wrap("back")
+
+local uuid = sha.digest(ni.getMetaOwner().id)
+
+while true do
+  local x,y,z = gps.locate()
+  local packet = {
+    sender=uuid,
+    type="gps",
+    data = {
+      x=x,
+      y=y,
+      z=z
+    }
+  }
+  local tPacket = textutils.serialize(packet)
+  local encData = enc.encrypt(tPacket,key)
+  modem.transmit(61312,61312,encData)
+end
